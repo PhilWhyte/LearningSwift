873652979fd4f10ca0328eb143732083dc8f2592
PhilWhyte <philipwhyte@hotmail.com>
2024-09-15 19:12:57 +0100

    Initial Commit

---
+++ LearningSwift/LearningSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftApp.swift
+//  LearningSwift
+//
+//  Created by Philip Whyte on 15/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
