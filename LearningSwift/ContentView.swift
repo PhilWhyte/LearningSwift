6d84146c62d76e2cc07767c2664c4cadeb05a0e8
PhilWhyte <philipwhyte@hotmail.com>
2024-09-15 19:22:47 +0100

    Version 1.0

--- LearningSwift/ContentView.swift
+++ LearningSwift/ContentView.swift
@@ -8,17 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = "This is nothing like the truth especially on a tuesday"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .padding()
+                .padding()
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .frame(height:150)
+                .frame(maxWidth: .infinity)
+                .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: 1)
+
+            HStack {
+                Button("Awesome") {
+                    //This is the action performed when the button is pressed
+                    messageString = "You are Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                Button("Great") {
+                    
+                    messageString = "You are Great"            }
+                .buttonStyle(.borderedProminent)
+                
+            }
         }
-        .padding()
+        
     }
 }
-
 #Preview {
     ContentView()
 }
